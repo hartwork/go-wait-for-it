b0d769b19e2ddf2b56d7739092612b2bd303a3c5
Sebastian Pipping <sebastian@pipping.org>
2020-11-07 01:51:11 +0100

    Add some software testing

---
+++ internal/cli/parser_test.go
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2020 Sebastian Pipping <sebastian@pipping.org>
+ * Licensed under AGPL v3 or later
+ */
+package cli
+
+import (
+	"testing"
+	"time"
+
+	"github.com/hartwork/go-wait-for-it/internal/syntax"
+	"github.com/stretchr/testify/assert"
+)
+
+func toAddress(t *testing.T, text string) syntax.Address {
+	address, err := syntax.ParseAddress(text)
+	assert.Nil(t, err)
+	return address
+}
+
+func TestParserWellFormed(t *testing.T) {
+	tests := []struct {
+		args           []string
+		expectedConfig Config
+	}{
+		{ // all default settings
+			[]string{},
+			Config{
+				nil,
+				[]string{},
+				false,
+				15 * time.Second,
+			},
+		}, { // cover as many non-defaults combined as possible
+			[]string{
+				"-q",
+				"-t", "2",
+				"-s", "h1:1",
+				"--service", "h2:2",
+				"--",
+				"echo", "hello",
+			}, Config{
+				[]syntax.Address{toAddress(t, "h1:1"), toAddress(t, "h2:2")},
+				[]string{"echo", "hello"},
+				true,
+				2 * time.Second,
+			},
+		}, { // cover remaining non-defaults
+			[]string{
+				"--quiet",
+				"--timeout", "2",
+				"echo", "hello",
+			}, Config{
+				nil,
+				[]string{"echo", "hello"},
+				true,
+				2 * time.Second,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		config := Parse(test.args)
+		assert.Equal(t, config, test.expectedConfig)
+	}
+}
---
+++ internal/syntax/address_test.go
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2020 Sebastian Pipping <sebastian@pipping.org>
+ * Licensed under AGPL v3 or later
+ */
+package syntax
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddressToString(t *testing.T) {
+	require.Equal(t,
+		Address{"hostname", 123}.String(),
+		"hostname:123",
+	)
+}
+
+func TestErrorToString(t *testing.T) {
+	require.Equal(t,
+		MalformedAddressError{"no port here"}.Error(),
+		"Malformed address: no port here",
+	)
+}
+
+func TestParserWellFormed(t *testing.T) {
+	tests := []struct {
+		candidate    string
+		expectedHost string
+		expectedPort uint16
+	}{
+		{"host:1", "host", 1},         // minimum port number
+		{"host:65535", "host", 65535}, // maximum port number
+		{":123", "", 123},             // hostname absent
+	}
+	for _, test := range tests {
+		address, err := ParseAddress(test.candidate)
+		assert.Nil(t, err)
+		assert.Equal(t, address.host, test.expectedHost)
+		assert.Equal(t, address.port, test.expectedPort)
+	}
+}
+
+func TestParserMalformed(t *testing.T) {
+	tests := []string{
+		"host",       // no port A
+		"host:",      // no port B
+		"host:foo",   // port not an integer
+		"host:0",     // below minimum port number
+		"host:65536", // above maximum port number
+		"host:01",    // forbidden leading zeros
+	}
+
+	for _, candidate := range tests {
+		_, err := ParseAddress(candidate)
+		assert.Equal(t, err.Error(), "Malformed address: "+candidate)
+	}
+}
