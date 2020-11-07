2e5adeece4964cb39afdd27af0a766551c434687
Sebastian Pipping <sebastian@pipping.org>
2020-11-07 19:32:01 +0100

    main_test.go: Extract port testing helper functions

--- main_test.go
+++ main_test.go
@@ -14,66 +14,74 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestWaitForAddress(t *testing.T) {
+func withAvailablePort(t *testing.T, actUpon func(syntax.Address)) {
 	listener, err := net.Listen("tcp", ":0")
 	require.Nil(t, err)
 	defer listener.Close()
 
 	address, err := syntax.ParseAddress(listener.Addr().String())
 	require.Nil(t, err)
-	port := address.Port
 
-	addresses := []syntax.Address{
-		{"localhost", port},
-		{"127.0.0.1", port},
-		{"[::]", port},
-		{"", port},
-	}
-
-	deadlineCombined := time.After(2 * time.Second)
-	for _, address := range addresses {
-		available := waitForAddress(address)
-		select {
-		case <-available:
-		case <-deadlineCombined:
-			t.Errorf("waitForAddress should be long done by now.")
-		}
-	}
+	actUpon(address)
 }
 
-func TestWaitForAddressWithTimeoutSuccess(t *testing.T) {
-	listener, err := net.Listen("tcp", ":0")
-	require.Nil(t, err)
-	defer listener.Close()
-
-	address, err := syntax.ParseAddress(listener.Addr().String())
-	assert.Nil(t, err)
-
-	timeout := 2 * time.Second
-	startedAt := time.Now()
-	results := make(chan connectResult)
-
-	go waitForAddressWithTimeout(address, timeout, startedAt, results)
-
-	result := <-results
-	assert.Nil(t, result.err)
-}
-
-func TestWaitForAddressWithTimeoutFailure(t *testing.T) {
+func withUnavailablePort(t *testing.T, actUpon func(syntax.Address)) {
 	listener, err := net.Listen("tcp", ":0")
 	require.Nil(t, err)
 
 	address, err := syntax.ParseAddress(listener.Addr().String())
-	assert.Nil(t, err)
+	require.Nil(t, err)
 
 	listener.Close()
 
-	timeout := 100 * time.Millisecond // small to not blow up test runtime
-	startedAt := time.Now()
-	results := make(chan connectResult)
+	actUpon(address)
+}
 
-	go waitForAddressWithTimeout(address, timeout, startedAt, results)
+func TestWaitForAddress(t *testing.T) {
+	withAvailablePort(t, func(address syntax.Address) {
+		port := address.Port
 
-	result := <-results
-	assert.NotNil(t, result.err)
+		addresses := []syntax.Address{
+			{"localhost", port},
+			{"127.0.0.1", port},
+			{"[::]", port},
+			{"", port},
+		}
+
+		deadlineCombined := time.After(2 * time.Second)
+		for _, address := range addresses {
+			available := waitForAddress(address)
+			select {
+			case <-available:
+			case <-deadlineCombined:
+				t.Errorf("waitForAddress should be long done by now.")
+			}
+		}
+	})
+}
+
+func TestWaitForAddressWithTimeoutSuccess(t *testing.T) {
+	withAvailablePort(t, func(address syntax.Address) {
+		timeout := 2 * time.Second
+		startedAt := time.Now()
+		results := make(chan connectResult)
+
+		go waitForAddressWithTimeout(address, timeout, startedAt, results)
+
+		result := <-results
+		assert.Nil(t, result.err)
+	})
+}
+
+func TestWaitForAddressWithTimeoutFailure(t *testing.T) {
+	withUnavailablePort(t, func(address syntax.Address) {
+		timeout := 100 * time.Millisecond // small to not blow up test runtime
+		startedAt := time.Now()
+		results := make(chan connectResult)
+
+		go waitForAddressWithTimeout(address, timeout, startedAt, results)
+
+		result := <-results
+		assert.NotNil(t, result.err)
+	})
 }
